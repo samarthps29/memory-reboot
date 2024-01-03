17a3f2510a33ef12ea2942079d0a16f8255f0c24
samarthps29 <samarthps29@gmail.com>
2024-01-03 20:28:47 +0530

    Redesigned the pop-up menu

--- components/Common/FloatingMenu.tsx
+++ components/Common/FloatingMenu.tsx
@@ -7,13 +7,14 @@ import {
 	MenuOptions,
 	MenuTrigger,
 } from "react-native-popup-menu";
-import { COLORS, SIZES } from "../../constants/theme";
+import { COLORS, FONT, SIZES } from "../../constants/theme";
 import { AudioContext } from "../../utils/Contexts/AudioContext";
 import { StorageContext } from "../../utils/Contexts/StorageContext";
 import { songItemType } from "../../utils/TypeDeclarations";
-import { View } from "./Themed";
-
-const Divider = () => <View style={styles.divider} />;
+import { View, Text } from "./Themed";
+import { renderers } from "react-native-popup-menu";
+import { reducedTitle } from "../../utils/global";
+const { SlideInMenu } = renderers;
 
 const FloatingMenu = ({
 	song,
@@ -29,7 +30,7 @@ const FloatingMenu = ({
 	const audioContext = useContext(AudioContext);
 	const colorScheme = useColorScheme();
 	return (
-		<Menu style={styles.container}>
+		<Menu style={styles.container} renderer={SlideInMenu}>
 			<MenuTrigger>
 				<Ionicons
 					name="reorder-four-outline"
@@ -45,9 +46,15 @@ const FloatingMenu = ({
 			<MenuOptions
 				customStyles={{
 					optionsContainer: {
-						backgroundColor: "transparent",
-						borderWidth: 0,
-						elevation: 0,
+						backgroundColor:
+							colorScheme === "light"
+								? COLORS.whiteSecondary
+								: COLORS.darkSecondary,
+						paddingHorizontal: 8,
+						paddingVertical: 8,
+						borderRadius: 0,
+						borderTopLeftRadius: 16,
+						borderTopRightRadius: 16,
 					},
 					optionsWrapper: {
 						elevation: 0,
@@ -56,195 +63,324 @@ const FloatingMenu = ({
 					},
 				}}
 			>
-				<View
-					style={{
-						flex: 1,
-						width: "90%",
-						borderRadius: SIZES.small,
-						overflow: "hidden",
-						marginTop: 28,
+				<MenuOption
+					customStyles={{
+						optionWrapper: [
+							styles.menuOption,
+							{
+								backgroundColor:
+									colorScheme === "light"
+										? COLORS.whiteSecondary
+										: COLORS.darkSecondary,
+							},
+						],
+					}}
+					onSelect={() => {
+						return false;
 					}}
 				>
-					{!renderPlaylists && (
-						<>
-							{storageContext?.playlistData &&
-								storageContext?.playlistData?.length > 1 && (
-									<MenuOption
-										onSelect={() => {
-											setRenderPlaylists(true);
-											return false;
-										}}
-										text="Add to Playlist"
-										customStyles={{
-											optionWrapper: styles.menuOption,
-										}}
-									/>
-								)}
+					<Text
+						numberOfLines={1}
+						style={[
+							styles.menuText,
+							{
+								color:
+									colorScheme === "light"
+										? "black"
+										: COLORS.whitePrimary,
+							},
+						]}
+					>
+						{reducedTitle(song.sname, 100)}
+					</Text>
+				</MenuOption>
+				{!renderPlaylists && (
+					<>
+						{storageContext?.playlistData &&
+							storageContext?.playlistData?.length > 1 && (
+								<MenuOption
+									onSelect={() => {
+										setRenderPlaylists(true);
+										return false;
+									}}
+									customStyles={{
+										optionWrapper: [
+											styles.menuOption,
+											{
+												backgroundColor:
+													colorScheme === "light"
+														? COLORS.whiteSecondary
+														: COLORS.darkSecondary,
+											},
+										],
+									}}
+								>
+									<Text
+										style={[
+											styles.menuText,
+											{
+												color:
+													colorScheme === "light"
+														? "black"
+														: COLORS.whitePrimary,
+											},
+										]}
+									>
+										Add to Playlist
+									</Text>
+								</MenuOption>
+							)}
 
-							{/* <Divider /> */}
+						<MenuOption
+							customStyles={{
+								optionWrapper: [
+									styles.menuOption,
+									{
+										backgroundColor:
+											colorScheme === "light"
+												? COLORS.whiteSecondary
+												: COLORS.darkSecondary,
+									},
+								],
+							}}
+							onSelect={() => {
+								audioContext?.setUserQueue((prev) => {
+									if (prev === null)
+										return {
+											queue: [song],
+											currentIndex: 0,
+										};
+									prev.queue.push(song);
+									if (prev.currentIndex === -1)
+										prev.currentIndex =
+											prev.queue.length - 1;
+									return prev;
+								});
+							}}
+						>
+							<Text
+								style={[
+									styles.menuText,
+									{
+										color:
+											colorScheme === "light"
+												? "black"
+												: COLORS.whitePrimary,
+									},
+								]}
+							>
+								Add to Queue
+							</Text>
+						</MenuOption>
+						{currPlaylist !== "0" && (
 							<MenuOption
-								text="Add to Queue"
 								customStyles={{
-									optionWrapper: styles.menuOption,
+									optionWrapper: [
+										styles.menuOption,
+										{
+											backgroundColor:
+												colorScheme === "light"
+													? COLORS.whiteSecondary
+													: COLORS.darkSecondary,
+										},
+									],
 								}}
 								onSelect={() => {
-									audioContext?.setUserQueue((prev) => {
-										if (prev === null)
-											return {
-												queue: [song],
-												currentIndex: 0,
-											};
-										prev.queue.push(song);
-										if (prev.currentIndex === -1)
-											prev.currentIndex =
-												prev.queue.length - 1;
-										return prev;
-									});
-								}}
-							/>
-							{audioContext?.showQueue !== "" && (
-								<>
-									{/* <Divider /> */}
-									<MenuOption
-										text="Remove from Queue"
-										customStyles={{
-											optionWrapper: styles.menuOption,
-										}}
-										onSelect={() => {
-											const selectedQueue =
-												audioContext?.showQueue;
-											if (
-												selectedQueue ===
-													"globalqueue" &&
-												audioContext?.globalQueue
-											) {
-												if (
-													index >
-													audioContext.globalQueue
-														.currentIndex
-												) {
-													// remove song from global queue
-													audioContext?.setGlobalQueue(
-														(prev) => {
-															if (prev === null)
-																return null;
-															prev.queue =
-																prev.queue.filter(
-																	(
-																		_,
-																		qindex
-																	) =>
-																		qindex !==
-																		index
-																);
-															return prev;
-														}
+									storageContext?.setSongData((prev) => {
+										return prev.map((item) => {
+											if (item.sid === song.sid) {
+												const filtered =
+													item.playlists.filter(
+														(pitem) =>
+															pitem !==
+															currPlaylist
 													);
-												}
-											} else if (
-												selectedQueue === "userqueue" &&
-												audioContext?.userQueue
-											) {
-												if (
-													index >
-													audioContext?.userQueue
-														?.currentIndex
-												) {
-													// remove song from user queue
-													audioContext.setUserQueue(
-														(prev) => {
-															if (prev === null)
-																return null;
-															prev.queue =
-																prev.queue.filter(
-																	(
-																		_,
-																		qindex
-																	) =>
-																		qindex !==
-																		index
-																);
-															return prev;
-														}
-													);
-												}
-											}
-										}}
-									/>
-								</>
-							)}
-							{currPlaylist !== "0" && (
-								<>
-									{/* <Divider /> */}
-									<MenuOption
-										text="Remove from Playlist"
-										customStyles={{
-											optionWrapper: styles.menuOption,
-										}}
-										onSelect={() => {
-											storageContext?.setSongData(
-												(prev) => {
-													return prev.map((item) => {
-														if (
-															item.sid ===
-															song.sid
-														) {
-															const filtered =
-																item.playlists.filter(
-																	(pitem) =>
-																		pitem !==
-																		currPlaylist
-																);
 
-															return {
-																...item,
-																playlists:
-																	filtered,
-															};
-														} else return item;
-													});
-												}
-											);
-											storageContext?.setSaveToggle(true);
-										}}
-									/>
-								</>
-							)}
-						</>
-					)}
-					{renderPlaylists &&
-						storageContext?.playlistData.map((item) => {
-							if (item.pid !== "0")
-								return (
-									<MenuOption
-										customStyles={{
-											optionWrapper: styles.menuOption,
-										}}
-										key={item.pid}
-										text={item.pname}
-										onSelect={() => {
-											storageContext.setSongData(
+												return {
+													...item,
+													playlists: filtered,
+												};
+											} else return item;
+										});
+									});
+									storageContext?.setSaveToggle(true);
+								}}
+							>
+								<Text
+									style={[
+										styles.menuText,
+										{
+											color:
+												colorScheme === "light"
+													? "black"
+													: COLORS.whitePrimary,
+										},
+									]}
+								>
+									Remove from Playlist
+								</Text>
+							</MenuOption>
+						)}
+						{audioContext?.showQueue !== "" && (
+							<MenuOption
+								customStyles={{
+									optionWrapper: [
+										styles.menuOption,
+										{
+											backgroundColor:
+												colorScheme === "light"
+													? COLORS.whiteSecondary
+													: COLORS.darkSecondary,
+										},
+									],
+								}}
+								onSelect={() => {
+									const selectedQueue =
+										audioContext?.showQueue;
+									if (
+										selectedQueue === "globalqueue" &&
+										audioContext?.globalQueue
+									) {
+										if (
+											index >
+											audioContext.globalQueue
+												.currentIndex
+										) {
+											// remove song from global queue
+											audioContext?.setGlobalQueue(
 												(prev) => {
-													return prev.map((ssong) => {
-														if (
-															song.sid ===
-															ssong.sid
-														) {
-															ssong.playlists.push(
-																item.pid
-															);
-														}
-														return ssong;
-													});
+													if (prev === null)
+														return null;
+													prev.queue =
+														prev.queue.filter(
+															(_, qindex) =>
+																qindex !== index
+														);
+													return prev;
 												}
 											);
-											storageContext.setSaveToggle(true);
-											setRenderPlaylists(false);
-										}}
-									/>
-								);
-						})}
-				</View>
+										}
+									} else if (
+										selectedQueue === "userqueue" &&
+										audioContext?.userQueue
+									) {
+										if (
+											index >
+											audioContext?.userQueue
+												?.currentIndex
+										) {
+											// remove song from user queue
+											audioContext.setUserQueue(
+												(prev) => {
+													if (prev === null)
+														return null;
+													prev.queue =
+														prev.queue.filter(
+															(_, qindex) =>
+																qindex !== index
+														);
+													return prev;
+												}
+											);
+										}
+									}
+								}}
+							>
+								<Text
+									style={[
+										styles.menuText,
+										{
+											color:
+												colorScheme === "light"
+													? "black"
+													: COLORS.whitePrimary,
+										},
+									]}
+								>
+									Remove from Queue
+								</Text>
+							</MenuOption>
+						)}
+					</>
+				)}
+				{renderPlaylists &&
+					storageContext?.playlistData.map((item) => {
+						if (item.pid !== "0")
+							return (
+								<MenuOption
+									customStyles={{
+										optionWrapper: [
+											styles.menuOption,
+											{
+												backgroundColor:
+													colorScheme === "light"
+														? COLORS.whiteSecondary
+														: COLORS.darkSecondary,
+											},
+										],
+									}}
+									key={item.pid}
+									onSelect={() => {
+										storageContext.setSongData((prev) => {
+											return prev.map((ssong) => {
+												if (song.sid === ssong.sid) {
+													ssong.playlists.push(
+														item.pid
+													);
+												}
+												return ssong;
+											});
+										});
+										storageContext.setSaveToggle(true);
+										setRenderPlaylists(false);
+									}}
+								>
+									<Text
+										style={[
+											styles.menuText,
+											{
+												color:
+													colorScheme === "light"
+														? "black"
+														: COLORS.whitePrimary,
+											},
+										]}
+									>
+										{item.pname}
+									</Text>
+								</MenuOption>
+							);
+					})}
+				<MenuOption
+					customStyles={{
+						optionWrapper: [
+							styles.menuOption,
+							{
+								backgroundColor:
+									colorScheme === "light"
+										? COLORS.whiteSecondary
+										: COLORS.darkSecondary,
+							},
+						],
+					}}
+					onSelect={() => {
+						setRenderPlaylists(false);
+					}}
+				>
+					<Text
+						style={[
+							styles.menuText,
+							{
+								color:
+									colorScheme === "light"
+										? "black"
+										: COLORS.whitePrimary,
+							},
+						]}
+					>
+						Cancel
+					</Text>
+				</MenuOption>
 			</MenuOptions>
 		</Menu>
 	);
@@ -256,14 +392,14 @@ const styles = StyleSheet.create({
 	container: {
 		backgroundColor: "transparent",
 	},
-	divider: {
-		height: StyleSheet.hairlineWidth,
-		backgroundColor: "#7F8487",
-	},
 	menuOption: {
-		paddingVertical: 10,
-		paddingHorizontal: SIZES.small,
-		backgroundColor: COLORS.whitePrimary,
+		paddingVertical: 13,
+		paddingHorizontal: SIZES.xSmall,
+		backgroundColor: COLORS.darkSecondary,
 		overflow: "scroll",
+	},
+	menuText: {
+		fontFamily: FONT.medium,
+		fontSize: 16,
 	},
 });
